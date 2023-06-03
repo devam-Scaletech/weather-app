dff60813ecac76c3b59f2ea4de1f6bea47919fdb
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-06-03 19:42:32 +0530

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+const App: React.FC = () => {
+	return <Routes></Routes>;
+};
+
+export default App;
---
+++ src/root.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from 'shared/components/errorBoundary/errorBoundary';
+import App from './app';
+import 'assets/styles/app.scss';
+
+const Root: React.FC = (props) => {
+	return (
+		<ErrorBoundary>
+			<BrowserRouter>
+				<App {...props} />
+			</BrowserRouter>
+		</ErrorBoundary>
+	);
+};
+
+export default Root;
