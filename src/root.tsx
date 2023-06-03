431bccf7755564dfb3c547629ebc47931e483669
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-06-03 22:14:18 +0530

    font: added fonts | core: styling change

--- src/root.tsx
+++ src/root.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ErrorBoundary from 'shared/components/errorBoundary/errorBoundary';
 import App from './app';
-import 'assets/styles/app.scss';
+import 'assets/styles/index.scss';
 
 const Root: React.FC = (props) => {
 	return (
