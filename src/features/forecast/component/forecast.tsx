c9d58e45b1d1636f2828304a7e6f9d2675bcce16
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-06-04 09:48:58 +0530

    feat: weather api integration done

--- src/features/forecast/component/forecast.tsx
+++ src/features/forecast/component/forecast.tsx
@@ -1,6 +1,46 @@
+import { useCallback, useEffect, useState } from 'react';
+import { API_CONFIG } from 'shared/constants/api';
+import httpService from 'shared/services/http.service';
 import ForecastDetails from './forecastDetails';
 import ForecastHeader from './forecastHeader';
+
 const Forecast = () => {
+	const [lat, setLat] = useState();
+	const [long, setLong] = useState();
+	const [weatherData, setWeatherData] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+	const API_key = process.env.REACT_APP_API_KEY;
+
+	const getCurrentLocation = useCallback(() => {
+		navigator.geolocation.getCurrentPosition(function (position: any) {
+			setLat(position.coords.latitude);
+			setLong(position.coords.longitude);
+		});
+	}, [lat, long]);
+
+	const getWeatherData = useCallback(() => {
+		setIsLoading(true);
+		httpService
+			.get(
+				`${'https://api.openweathermap.org/data/2.5'}/${
+					API_CONFIG.path.weather
+				}?lat=${lat}&lon=${long}&units=metric&APPID=${API_key}`
+			)
+			.then((response) => {
+				setWeatherData(response);
+				setIsLoading(false);
+			})
+			.catch((error) => {
+				setIsLoading(false);
+				console.error('error', error);
+			});
+	}, [lat, long]);
+
+	useEffect(() => {
+		getWeatherData();
+		getCurrentLocation();
+	}, [lat, long]);
+
 	return (
 		<div className='flex position--relative'>
 			<ForecastHeader />
--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -2,11 +2,9 @@ import queryString from 'query-string';
 import isEmpty from 'lodash/isEmpty';
 
 export const API_CONFIG = {
-	baseUrl: `${process.env.REACT_APP_BASE_URL}`,
+	baseUrl: process.env.REACT_APP_BASE_URL,
 	path: {
-		login: 'login',
-		forget: 'password/forgot',
-		reset: 'password/reset'
+		weather: 'weather'
 	}
 };
 
